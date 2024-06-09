533b5968b14fcf42fbe10b7ceb0c45e99ba3a1ce
tvson <01263519039son@gmail.com>
2024-06-09 14:34:35 +0700

    Submit code from day16 to day29

--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -1,9 +1,12 @@
+
 package driver;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.ios.IOSDriver;
 import java.net.URL;
+import java.time.Duration;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class DriverFactory {
@@ -13,10 +16,10 @@ public class DriverFactory {
         // DesiredCaps
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
-        desiredCapabilities.setCapability(MobileCapabilityType.UDID, "emulator-5554");
-        desiredCapabilities.setCapability(MobileCapabilityType.APP_PACKAGE, "com.wdiodemoapp");
-        desiredCapabilities.setCapability(MobileCapabilityType.APP_ACTIVITY,
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME_OPTION, "uiautomator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.UDID_OPTION, "emulator-5554");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP_PACKAGE_OPTION, "com.wdiodemoapp");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP_ACTIVITY_OPTION,
                 "com.wdiodemoapp.MainActivity");
         URL appiumServer = null;
         try {
@@ -38,6 +41,44 @@ public class DriverFactory {
         }
 
         // Need one more thing here that we will talk in next lesson
+        // Global wait time applied for the WHOLE driver session - Implicit wait
+        appiumDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2L));
+        return appiumDriver;
+    }
+
+    public static AppiumDriver getDriver(Platform platform, String systemPort, String udid) {
+        AppiumDriver appiumDriver = null;
+        // DesiredCaps
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME_OPTION, "uiautomator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.SYSTEM_PORT, systemPort);
+        desiredCapabilities.setCapability(MobileCapabilityType.UDID_OPTION, udid);
+        desiredCapabilities.setCapability(MobileCapabilityType.APP_PACKAGE_OPTION, "com.wdiodemoapp");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP_ACTIVITY_OPTION,
+                "com.wdiodemoapp.MainActivity");
+        URL appiumServer = null;
+        try {
+            appiumServer = new URL("http://localhost:4723");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (appiumServer == null) {
+            throw new RuntimeException("Can't construct the appium server URL");
+        }
+
+        switch (platform) {
+            case ANDROID:
+                appiumDriver = new AndroidDriver(appiumServer, desiredCapabilities);
+                break;
+            case IOS:
+                appiumDriver = new IOSDriver(appiumServer, desiredCapabilities);
+                break;
+        }
+
+        // Need one more thing here that we will talk in next lesson
+        // Global wait time applied for the WHOLE driver session - Implicit wait
+        appiumDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2L));
         return appiumDriver;
     }
 
---
+++ src/test/java/tests/authen/AuthenticationTest.java
@@ -0,0 +1,28 @@
+package tests.authen;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import test_data.DataObjectBuilder;
+import test_data.models.LoginCred;
+import test_flows.authentication.LoginFlow;
+import tests.BaseTest;
+
+public class AuthenticationTest extends BaseTest {
+
+    @Test(dataProvider = "loginCredData")
+    public void loginWithCreds(LoginCred loginCred) {
+        LoginFlow loginFlow = new LoginFlow(
+                appiumDriver, loginCred.getEmail(), loginCred.getPassword()
+        );
+        loginFlow.gotoLoginScreen();
+        loginFlow.login();
+        loginFlow.verifyLogin();
+    }
+
+    @DataProvider
+    public LoginCred[] loginCredData() {
+        String loginCredDataPath = "/src/test/java/test_data/authen/LoginCredData.json";
+        return DataObjectBuilder.buildDataObject(loginCredDataPath, LoginCred[].class);
+    }
+
+}
